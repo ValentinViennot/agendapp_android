afa0a9b782108b595653b65b95d7d2c8fe0ec9bb
Valentin <misterw97@users.noreply.github.com>
2017-03-10 19:37:12 +0100

    Test Git

--- app/src/main/java/fr/agendapp/app/pages/ArchivesPage.java
+++ app/src/main/java/fr/agendapp/app/pages/ArchivesPage.java
@@ -1,5 +1,8 @@
 package fr.agendapp.app.pages;
 
+/**
+* Affichage des archives plutot que des devoirs à venir
+*/
 public class ArchivesPage extends WorkPage {
 
     static String type = "archives";
