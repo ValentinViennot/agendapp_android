a9ffd1832d96d56a968b5c5751fd259f737de634
misterw97 <tinodu78@gmail.com>
2017-04-04 01:59:59 +0200

    LoginPage

--- app/src/main/java/fr/agendapp/app/pages/LoginPage.java
+++ app/src/main/java/fr/agendapp/app/pages/LoginPage.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import fr.agendapp.app.App;
 import fr.agendapp.app.R;
+import fr.agendapp.app.factories.NotificationFactory;
 import fr.agendapp.app.factories.SyncFactory;
 
 public class LoginPage extends AppCompatActivity {
@@ -15,25 +17,30 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO Layout / Vue correspondant à cette activité
-        setContentView(R.layout.activity_main);
-        Log.i(App.TAG, "LoginPage created");
-        // TODO formulaire
-        SyncFactory.login(this, "test@agendapp.fr", "test");
+        setContentView(R.layout.activity_login);
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SyncFactory.login(LoginPage.this,
+                        username.getText().toString(),
+                        password.getText().toString());
+            }
+        });
     }
 
     public void onLogin(String token, String message) {
-        // TODO Notification.add message
         SharedPreferences preferences = getSharedPreferences(App.TAG, MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("token", token);
         editor.apply();
         Intent page = new Intent(LoginPage.this, App.class);
+        NotificationFactory.add(this, 0, message, "");
         startActivity(page);
     }
 
     public void onErrorLogin(String message) {
-        // TODO
-        Log.w(App.TAG, "message : " + message);
+        NotificationFactory.add(this, 2, message, "");
     }
 }
